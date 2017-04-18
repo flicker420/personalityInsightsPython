a3519d5f22b9f874dd67f21024aa3373d9ddae03
flicker420 <bluemix.dummy07@gmail.com>
2017-04-18 15:27:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,28 @@
+
+# Twitter Credentials
+#
+# To obtain the credentials, you must first have a Twitter account. 
+# Then, go to https://dev.twitter.com/, login, and click on "Manage Your Apps" to reach https://apps.twitter.com/.
+# Then, click "Create New App", fill in the relevant fields, and click "Create your Twitter application".
+# With the application created, navigate to the API Keys page and click "Create my access token". 
+# You now have the four necessary credentials. Copy the API key, API secret, 
+# Access token, and Access token secret here.
+# NOTE: API key and API secret go in the twitter_consumer_key and twitter_consumer_secret vars.
+#
+
+twitter_consumer_key = 'qYq4xCLlQUHfQ55X7Jf2WTYVh'
+twitter_consumer_secret = 'WxKKC24aw5DQPgbhDRRgCSufRT0LuZ1H4Toy2eVvmj9HBYBo4C'
+twitter_access_token = '2426929975-7NJQ69j60tpksWuq5HKyGmN8k4NLLnsafBXMHNq'
+twitter_access_secret = 'ShbgA5YTgo0RxTjFCGikWYHbFfbtmnUcZOuHOoHDEsbLY'
+
+# Personality Insights credentials and URL
+#
+# You can obtain these credentials by binding a PI service to an application in bluemix and 
+# and clicking the "show credentials" link on the service in the application dashboard.
+# Or you can use "cf env <application name>" from the command line to get the credentials.
+
+pi_url = 'https://gateway.watsonplatform.net/personality-insights/api'
+pi_username = '11cc1ab3-06bc-4e59-975f-edcda7e167f4'
+pi_password = 'LJOEw66hNhPc'
+
+
